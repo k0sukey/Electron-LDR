5f187d7e967ec85dd43c34e0f55f69f84704aa92
Kosuke Isobe <naughtydog0721@gmail.com>
2015-09-18 18:49:34 +0900

    ソートとフィルタを実装

--- app/app.js
+++ app/app.js
@@ -6,9 +6,67 @@ var _ = require('lodash'),
 	Cookie = require('../cookie'),
 	Feeds = require('../component/feeds');
 
-var sidebar;
+var feeds = [],
+	order = 'modified_on',
+	filter = '';
 
-var render = function(){
+function doOrder() {
+	var element = document.getElementById('order'),
+		index = element.selectedIndex;
+
+	order = element.options[index].value;
+
+	render(false);
+}
+
+function doFilter() {
+	var element = document.getElementById('filter');
+
+	filter = element.value;
+
+	render(false);
+}
+
+function render() {
+	var _feeds = feeds;
+
+	if (filter !== '') {
+		var regex = new RegExp(filter, 'i');
+
+		_feeds = _.filter(_feeds, function(feed){
+			return regex.test(feed.title);
+		});
+	}
+
+	feeds = feeds.sort(function(a, b){
+		switch (order) {
+			case 'modified_on':
+				return b.modified_on - a.modified_on;
+			case 'modified_on:reverse':
+				return a.modified_on - b.modified_on;
+			case 'unread_count':
+				return b.unread_count - a.unread_count;
+			case 'unread_count:reverse':
+				return a.unread_count - b.unread_count;
+			case 'title:reverse':
+				return a.title - b.title;
+			case 'rate':
+				return b.rate - a.rate;
+			case 'subscribers_count':
+				return b.subscribers_count - a.subscribers_count;
+			case 'subscribers_count:reverse':
+				return a.subscribers_count - b.subscribers_count;
+		}
+	});
+
+	React.unmountComponentAtNode(document.querySelector('#feeds'));
+
+	React.render(React.createElement(Feeds, {
+		feeds: _feeds,
+	}), document.querySelector('#feeds'));
+}
+
+function fetch(reload) {
 	request.post('http://reader.livedoor.com/api/subs', {
 		headers: {
 			'User-Agent': remote.getCurrentWindow().useragent,
@@ -22,24 +80,17 @@ var render = function(){
 			return;
 		}
 
-		var json;
-
 		try {
-			json = JSON.parse(body).sort(function(a, b){
-				return b.unread_count - a.unread_count;
-			});
-
-			React.unmountComponentAtNode(document.querySelector('#feeds'));
-
-			React.render(React.createElement(Feeds, {
-				feeds: json,
-			}), document.querySelector('#feeds'));
+			feeds = JSON.parse(body);
+			render();
 		} catch (e) {
 			Cookie.set('');
 			remote.getCurrentWindow().close();
 		}
 	});
-};
-render();
+}
+fetch(true);
 
-mousetrap.bind('r', render);
+mousetrap.bind('r', function(){
+	fetch(true);
+});
