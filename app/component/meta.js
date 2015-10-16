efe408f4f9e5ad7c29fafd8af7628da223a5679d
Kosuke Isobe <naughtydog0721@gmail.com>
2015-10-16 09:04:39 +0900

    レーティングを変更したらリロードするように変更

--- app/component/meta.js
+++ app/component/meta.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var _ = require('lodash'),
+    remote = require('remote'),
     request = require('request'),
+    ipc = remote.require('ipc'),
     React = require('react'),
     ReactRater = require('react-rater'),
     Cookie = require('../cookie');
@@ -44,11 +46,11 @@ module.exports = React.createClass({
 				return;
 			}
 
-			var feed = _.filter(json, (function (item) {
+			var feeds = _.filter(json, (function (item, index) {
 				return item.subscribe_id === this.props.feed.subscribe_id;
 			}).bind(this));
 
-			if (feed.length === 0) {
+			if (feeds.length === 0) {
 				return;
 			}
 
@@ -61,7 +63,23 @@ module.exports = React.createClass({
 					subscribe_id: this.props.feed.subscribe_id,
 					rate: rating
 				}
-			}, function (error, response, body) {});
+			}, function (error, response, body) {
+				if (error) {
+					return;
+				}
+
+				var json;
+
+				try {
+					json = JSON.parse(body);
+				} catch (e) {
+					return;
+				}
+
+				if (json.isSuccess) {
+					ipc.emit('reload');
+				}
+			});
 		}).bind(this));
 	},
 	render: function render() {
